68928eae6af564e507ab1e5b56318aa1b597e47a
Paulo Victor <37181462+paulovictor11@users.noreply.github.com>
2022-04-22 21:24:33 -0300

    Tela de entrada

---
+++ src/libs/theme.ts
@@ -0,0 +1,44 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    initialColorMode: 'light',
+};
+
+const theme = extendTheme({
+    config,
+    fonts: {
+        default: "Poppins, system-ui, sans-serif",
+    },
+    fontWeights: {
+        normal: 400,
+        medium: 600,
+        bold: 700,
+    },
+    radii: {
+        sm: '5px',
+        md: '8px',
+    },
+    fontSizes: {
+        '2xl': '54px',
+    },
+    colors: {
+        purple: {
+            50: '#e559f9',
+            500: '#835AFD',
+            700: '#6F4BD8'
+        },
+        red: {
+            500: '#E73F5D',
+            700: '#D73754',
+        },
+        gray: {
+            50: '#F8F8F8',
+            300: '#e1e1e6',
+            600: '#29292e',
+            700: '#202024',
+            800: '#121214',
+        }
+    }
+});
+
+export default theme;
